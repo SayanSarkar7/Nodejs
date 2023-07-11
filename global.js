12fd52a175df990cbe24a76aa415067dd9d50dcc
SayanSarkar7 <sarkarsayan602@gmail.com>
2023-07-11 13:36:51 +0530

    updated global.js

--- global.js
+++ global.js
@@ -1,12 +1,14 @@
-setTimeout(()=>{
-    console.log('in the timeout');
-},3000);
+// setTimeout(()=>{
+//     console.log('in the timeout');
+//     clearInterval(int);
+// },3000);
 
-const int = setInterval(()=>{
-    console.log('hello');
-},1000)
+// const int = setInterval(()=>{
+//     console.log('hello');
+// },1000)
 
-
+console.log(__dirname); // full absolute path of the directory
+console.log(__filename); // full absolute path of the directory with file name
 
 
 
