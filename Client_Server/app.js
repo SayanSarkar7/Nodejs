1315f625b54c8ed921b210d30a7050ea5ef66549
SayanSarkar7 <sarkarsayan602@gmail.com>
2023-07-12 16:05:18 +0530

    updated app.js

--- Client_Server/app.js
+++ Client_Server/app.js
@@ -3,19 +3,24 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+ app.set('view engine', 'ejs');
+
 // listen for request
 app.listen(3000);
 
 app.get('/', (req, res) => {
     
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', {root:__dirname});
+    // res.sendFile('./views/index.html', {root:__dirname});
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
     
     // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', {root:__dirname});
+    // res.sendFile('./views/about.html', {root:__dirname});
+    res.render('about');
 
 });
 
@@ -26,5 +31,6 @@ app.get('/about-us', (req, res)=>{
 
 // 404 page
 app.use((req, res)=>{
-    res.status(404).sendFile('./views/404.html', {root:__dirname})
+    // res.status(404).sendFile('./views/404.html', {root:__dirname})
+    res.status(404).render('404');
 })
